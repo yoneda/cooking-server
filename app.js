20debfb552b4507f4abefbde48f6fb14e1a12cc4
yoneda <nsshowdream@gmail.com>
2019-03-17 01:34:43 +0900

    change db setting

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ const delayObtainTasks = () => {
   return new Promise((resolve,reject)=>{
     const mysql = require("mysql");
     const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "tododb"
+      host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "a6gy1hq79u3dsfkr",
+      password: "ios34vdfh0jbsoj3",
+      database: "kqp69lp9xo2uinaf",
+      port:"3306",
     });
     connection.connect();
     connection.query("select * from tasks",(err,results,fields)=>{
@@ -35,10 +36,11 @@ const delayUpdateTask = (id,done) => {
   return new Promise((resolve,reject)=>{
     const mysql = require("mysql");
     const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "tododb"
+      host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "a6gy1hq79u3dsfkr",
+      password: "ios34vdfh0jbsoj3",
+      database: "kqp69lp9xo2uinaf",
+      port:"3306",
     });
     connection.connect();
     connection.query("update tasks set done = ? where id = ?",[done,id],(err,results,fields)=>{
@@ -53,10 +55,11 @@ const delayAddTask = (name) => {
   return new Promise((resolve,reject)=>{
     const mysql = require("mysql");
     const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "tododb"
+      host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "a6gy1hq79u3dsfkr",
+      password: "ios34vdfh0jbsoj3",
+      database: "kqp69lp9xo2uinaf",
+      port:"3306",
     });
     connection.connect();
     connection.query("insert into tasks(name, done, removed) values(?, false, false)",[name],(err,results,fields)=>{
@@ -71,10 +74,11 @@ const delayRemoveTask = (id) => {
   return new Promise((resolve,reject)=>{
     const mysql = require("mysql");
     const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "tododb"
+      host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "a6gy1hq79u3dsfkr",
+      password: "ios34vdfh0jbsoj3",
+      database: "kqp69lp9xo2uinaf",
+      port:"3306",
     });
     connection.connect();
     connection.query("update tasks set removed = true where id = ?",[id],(err,results,fields)=>{
