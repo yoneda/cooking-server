5ca3f4281246dcd5508cbb43c547ac5f11a6907a
yoneda <nsshowdream@gmail.com>
2019-11-26 23:06:49 +0900

    [clean] use await keyword

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-'use strict';
-const koa = require('koa');
-const koaRouter = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const json = require('koa-json');
-const cors = require('@koa/cors');
+"use strict";
+const koa = require("koa");
+const koaRouter = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const json = require("koa-json");
+const cors = require("@koa/cors");
 
 const app = new koa();
 const router = new koaRouter();
@@ -18,24 +18,20 @@ app.use(json());
 app.use(bodyParser());
 
 // すべてのユーザ情報を取得
-router.get("/users",async(ctx,next)=>{
-  const usersPromise = new Promise(resolve => {
+router.get("/users", async (ctx, next) => {
+  const results = await new Promise(resolve => {
     const mysql = require("mysql");
     const config = require("./dbconfig");
-    console.log(config);
     const connection = mysql.createConnection(config);
     connection.connect();
-    connection.query("select * from users",(errors, results,fields)=>{
+    connection.query("select * from users", (errors, results, fields) => {
       resolve(results);
-    })
+    });
     connection.end();
-  })
-  usersPromise.then(results=>{
-    results.forEach(ele=>console.log(ele.account));
   });
-  ctx.body = {hoge:"hogeee"};
-})
+  ctx.body = {hoge:results};
+});
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(process.env.PORT || 3000, ()=>console.log(process.env.PORT));
+app.listen(process.env.PORT || 3000, () => console.log(process.env.PORT));
