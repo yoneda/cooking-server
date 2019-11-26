ce77fec45b6fcb08dae0f840cf311313eb24ab1f
yoneda <nsshowdream@gmail.com>
2019-11-26 22:30:57 +0900

    [fix] make it possible to get users

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const cors = require('@koa/cors');
 const app = new koa();
 const router = new koaRouter();
 
-const delayObtainTasks = require('./utils/delayObtainTasks.js');
-const delayUpdateTask = require('./utils/delayUpdateTask.js');
-const delayAddTask = require('./utils/delayAddTask.js');
-const delayRemoveTask = require('./utils/delayRemoveTask.js');
-
 // corsを許可
 app.use(cors());
 
@@ -22,35 +17,23 @@ app.use(json());
 // postのパラメータをctx.request.body に挿入する
 app.use(bodyParser());
 
-// すべてのタスクを取得
-router.get("/tasks",async(ctx,next)=>{
-  const tasks = await delayObtainTasks();
-  ctx.body = {tasks:tasks};
-})
-
-// あるタスクを完了状態にする
-router.post("/tasks",async(ctx,next)=>{
-  const id = ctx.request.body.id;
-  const done = ctx.request.body.done;
-  const status = await delayUpdateTask(id,done);
-  const tasks = await delayObtainTasks();
-  ctx.body = {tasks:tasks};
-})
-
-// あるタスクを追加する
-router.post("/tasks/add",async(ctx,next)=>{
-  const name = ctx.request.body.name;
-  const status = await delayAddTask(name);
-  const tasks = await delayObtainTasks();
-  ctx.body = {tasks:tasks};
-})
-
-// あるタスクを削除する
-router.post("/tasks/remove",async(ctx,next)=>{
-  const id = ctx.request.body.id;
-  const status = await delayRemoveTask(id);
-  const tasks = await delayObtainTasks();
-  ctx.body = {tasks:tasks};
+// すべてのユーザ情報を取得
+router.get("/users",async(ctx,next)=>{
+  const usersPromise = new Promise(resolve => {
+    const mysql = require("mysql");
+    const config = require("./dbconfig");
+    console.log(config);
+    const connection = mysql.createConnection(config);
+    connection.connect();
+    connection.query("select * from users",(errors, results,fields)=>{
+      resolve(results);
+    })
+    connection.end();
+  })
+  usersPromise.then(results=>{
+    results.forEach(ele=>console.log(ele.account));
+  });
+  ctx.body = {hoge:"hogeee"};
 })
 
 app.use(router.routes()).use(router.allowedMethods());
