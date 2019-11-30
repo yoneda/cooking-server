2ac7190321fd66861b52d5edbfb39df4da24ac7e
yoneda <nsshowdream@gmail.com>
2019-11-30 19:59:35 +0900

    [clean] add some comments on controllers.recipes

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -1,5 +1,22 @@
 const db = require("../db");
 
+// MEMO: group_concatの部分を関数化すると綺麗にかける
+// MEMO: 以下のSQL文と同等
+/*
+SELECT r.*, u.account, GROUP_CONCAT(d.text SEPARATOR ",") AS directions, GROUP_CONCAT(b_r_i.name SEPARATOR ",") AS ingredients
+FROM recipes AS r
+LEFT JOIN users AS u ON r.user = u.id
+LEFT JOIN directions AS d ON r.id = d.recipe
+LEFT JOIN
+(
+SELECT r_i.id, r_i.recipe, i.name FROM recipes_ingredients AS r_i
+INNER JOIN ingredients as i on r_i.ingredient = i.id
+)
+AS b_r_i
+ON r.id = b_r_i.recipe
+GROUP BY r.id;
+*/
+
 const getRecipes = async ctx => {
   const ingredientsQuery = db("recipes_ingredients as r_i")
     .select("r_i.id", "r_i.recipe", "i.name")
