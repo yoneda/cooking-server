3e964beb9a2f72e399f9e69708664ab76d1bf6e4
yoneda <nsshowdream@gmail.com>
2019-12-01 04:20:58 +0900

    [add] add migration

---
+++ knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+
+module.exports = {
+  /*
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },*/
+
+  development: {
+    client: "mysql",
+    connection: {
+      host: "tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      database: "pa4cpcclmf6ps1ff",
+      user: "tquwcfdj27o9faux",
+      password: "m9brv58bygvynqwf"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./migrations",
+      tableName: "init"
+    }
+  },
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
---
+++ migrations/20191201035037_init.js
@@ -0,0 +1,50 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", table => {
+      table.increments("id");
+      table.string("account");
+      table.string("name");
+      table.string("bio");
+      table.string("mail");
+      table.string("password");
+    })
+    .createTable("recipes", table => {
+      table.increments("id");
+      table.string("title");
+      table.datetime("createdAt");
+      table.datetime("updatedAt");
+      table.integer("cookTime");
+      table.integer("cost");
+      table.integer("user");
+    })
+    .createTable("directions", table => {
+      table.increments("id");
+      table.string("text");
+      table.integer("recipe");
+      table.integer("process");
+    })
+    .createTable("ingredients", table => {
+      table.increments("id");
+      table.string("name");
+    })
+    .createTable("recipes_ingredients", table => {
+      table.increments("id");
+      table.integer("recipe");
+      table.integer("ingredient");
+    })
+    .createTable("stars", table => {
+      table.increments("id");
+      table.integer("user");
+      table.integer("recipe");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable("users")
+    .dropTable("recipes")
+    .dropTable("directions")
+    .dropTable("ingredients")
+    .dropTable("recipes_ingredients")
+    .dropTable("stars");
+};
