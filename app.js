f95eb73b3359a8eecf02ace6d3a859c43d726d2c
yoneda <nsshowdream@gmail.com>
2019-11-30 20:39:25 +0900

    [clean] remove unused code

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const koaRouter = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const json = require("koa-json");
 const cors = require("@koa/cors");
-
+const routes = require("./routes");
 const app = new koa();
 const router = new koaRouter();
+
 
 // corsを許可
 app.use(cors());
@@ -19,11 +20,5 @@ app.use(bodyParser());
 
 // TODO: 必須のリクエストパラメータがあるか考える
 
-const routes = require("./routes");
-
-console.log(routes);
-
 app.use(routes).use(router.allowedMethods());
-app.use(router.routes()).use(router.allowedMethods());
-
 app.listen(process.env.PORT || 3000, () => console.log(process.env.PORT));
