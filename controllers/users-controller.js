fe1c25fd4608d1e6ff8818e09e1c11fdc5f9bf60
yoneda <nsshowdream@gmail.com>
2019-11-30 20:36:58 +0900

    [clean] move some endpoints

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,3 +1,4 @@
+const { pickBy } = require("lodash");
 const db = require("../db");
 
 const getUsers = async ctx => {
@@ -15,7 +16,48 @@ const getOneUser = async ctx => {
   ctx.body = { user };
 };
 
+// TODO: HttpResponce を201にする
+const postUsers = async ctx => {
+  const { account, mail, pass } = ctx.query;
+  const [id] = await db.into("users").insert({ account, mail, pass });
+  const [user] = await db
+    .select()
+    .from("users")
+    .where({ id });
+  ctx.body = { user: user };
+};
+
+// TODO: HttpResponce を201にする
+// TODO: HttpメソッドのPUTは、リソースを更新してなければ作成、と定義されているがそうなってはいない
+const putUser = async ctx => {
+  const { account } = ctx.params;
+  const clearQuery = pickBy(ctx.query, value => value !== undefined);
+
+  await db("users")
+    .where({ account })
+    .update(clearQuery);
+
+  const [user] = await db
+    .select()
+    .from("users")
+    .where({ account })
+    .limit(1);
+
+  ctx.body = { user };
+};
+
+const delUser = async ctx => {
+  const { account } = ctx.params;
+  const num = await db("users")
+    .where({ account })
+    .del();
+  ctx.body = { success: num > 0 };
+};
+
 module.exports = {
   get: getUsers,
   getOne: getOneUser,
+  post: postUsers,
+  put: putUser,
+  del: delUser
 };
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -4,5 +4,8 @@ const router = new Router();
 
 router.get("/users", controller.users.get);
 router.get("/users/:account", controller.users.getOne);
+router.post("/users", controller.users.post);
+router.put("/users/:account", controller.users.put);
+router.del("/users/:account", controller.users.del);
 
 module.exports = router.routes();
