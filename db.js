564ab19827d7e54bc3d88f8ddd758c5b8039132e
yoneda <nsshowdream@gmail.com>
2019-12-02 10:44:12 +0900

    [fix] use knexfile.js

--- db.js
+++ db.js
@@ -1,12 +1,4 @@
-const knexConfig = {
-  client: "mysql",
-  connection: {
-    host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "a6p9p4y8675jagbb",
-    password: "vh2sf4kiobbbdmy7",
-    database: "do6sfdqn1wbkjayj"
-  }
-};
-const db = require("knex")(knexConfig);
+const config = require("./knexfile");
+const db = require("knex")(config.development);
 
 module.exports = db;
