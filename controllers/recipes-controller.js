1f8e11be8fc99dbc3df714ebe13308056d38dbce
yoneda <nsshowdream@gmail.com>
2019-12-06 23:55:49 +0900

    [fix] fix sql on recipes-controller.get

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -5,7 +5,7 @@ const { pick, pickBy, identity } = require("lodash");
 // MEMO: group_concatの部分を関数化すると綺麗にかける
 // MEMO: 以下のSQL文と同等
 /*
-SELECT r.*, u.account, GROUP_CONCAT(d.text SEPARATOR ",") AS directions, GROUP_CONCAT(b_r_i.name SEPARATOR ",") AS ingredients
+SELECT r.*, u.account, GROUP_CONCAT(DISTINCT(d.text) SEPARATOR ",") AS directions, GROUP_CONCAT(DISTINCT(b_r_i.name) SEPARATOR ",") AS ingredients
 FROM recipes AS r
 LEFT JOIN users AS u ON r.user = u.id
 LEFT JOIN directions AS d ON r.id = d.recipe
@@ -19,6 +19,8 @@ ON r.id = b_r_i.recipe
 GROUP BY r.id;
 */
 
+// TODO: group_concat(distinct(…))のあたりの文法が怪しい。SQLの勉強し直す
+
 const getRecipes = async ctx => {
   const ingredientsQuery = db("recipes_ingredients as r_i")
     .select("r_i.id", "r_i.recipe", "i.name")
@@ -28,8 +30,8 @@ const getRecipes = async ctx => {
     .select(
       "r.*",
       "u.account",
-      db.raw("group_concat(d.text separator ',') as directions"),
-      db.raw("group_concat(b_r_i.name separator ',') as ingredients")
+      db.raw("group_concat(distinct(d.text) separator ',') as directions"),
+      db.raw("group_concat(distinct(b_r_i.name) separator ',') as ingredients")
     )
     .leftJoin("users as u", "r.user", "u.id")
     .leftJoin("directions as d", "r.id", "d.recipe")
