365fa5302e6e7020e9137d3b070dc35f0b7b7e3a
yoneda <nsshowdream@gmail.com>
2019-12-16 23:47:39 +0900

    [fix] make it possible to emit jwt token when user login

--- controllers/users-auth-controller.js
+++ controllers/users-auth-controller.js
@@ -1,4 +1,6 @@
 const db = require("../db");
+const jwt = require("jsonwebtoken");
+const { pick } = require("lodash");
 
 const loginUser = async ctx => {
   const { account } = ctx.query;
@@ -7,8 +9,16 @@ const loginUser = async ctx => {
     .from("users")
     .where({ account })
     .limit(1);
-  const nextUser = { ...user, token: "tokentokentoken" };
-  ctx.body = { user: nextUser };
+
+  const secret = process.env.SECRET;
+  const token = jwt.sign(
+    { user: pick(user, ["account", "mail", "password"]) },
+    secret,
+    {
+      expiresIn: "60000"
+    }
+  );
+  ctx.body = { user: { ...user, token } };
 };
 
 module.exports = {
